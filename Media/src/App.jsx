6ad8badc2cc4df5c47748bebca1cf16b59aa55ea
eden shabi <eden9876102@gmail.com>
2024-11-28 20:13:47 +0200

    update

--- Media/src/App.jsx
+++ Media/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import SignInPage from "./Pages/SignInPage";
 import Home from "./Pages/Home";
 import SignUp from "./Pages/SignUp";
@@ -10,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SignInPage />} />
+        <Route path="/" element={<Navigate to={"/sign-in"} replace />} />
         <Route path="/home" element={<Home />} /> {/* נתיב נוסף לדף הבית */}
         <Route path="/sign-in" element={<SignInPage />} />
         {/* נתיב לעמוד ההתחברות */}
